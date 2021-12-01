f473ab2feac9bf4fc13e498125f7406b04dabb9f
Fermin Aguilar <ferminab99@gmail.com>
2021-12-01 18:30:09 -0300

    se agregaron detalles de animacion en el titulo del sitio

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import Titulo from './components/common/Titulo';
-import Buscador from './components/common/Buscador';
-import Apple from './components/pages/Apple';
+import Buscador from './components/common/Buscador'
 
 
 
@@ -13,7 +12,7 @@ function App() {
       <Titulo/>
       <Buscador/>
       <Routes>
-        
+
       </Routes>
     </Router>
 
--- src/components/common/Titulo.js
+++ src/components/common/Titulo.js
@@ -3,8 +3,8 @@ import '../../App.css';
 
 const Titulo = () => {
     return (
-        <div className='text-center display-4 pt-4 bg-light titulo'>
-            <h1 className='display-4' >Buscador de noticias</h1>
+        <div className='text-center display-4 pt-4  fondo'>
+            <h1 className='display-4 titulo' >Buscador de noticias</h1>
             <hr />
         </div>
     );
